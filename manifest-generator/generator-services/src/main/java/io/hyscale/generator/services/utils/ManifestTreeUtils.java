db36641d377c830e618ea0ac0ed87b3f636a39d0
sameerag <sameera.grandhi@hyscale.io>
2019-11-12 11:56:33 +0530

    Fix for merging array snippet elements into existing array

--- manifest-generator/generator-services/src/main/java/io/hyscale/generator/services/utils/ManifestTreeUtils.java
+++ manifest-generator/generator-services/src/main/java/io/hyscale/generator/services/utils/ManifestTreeUtils.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,112 +33,116 @@ import io.hyscale.commons.utils.ObjectMapperFactory;
 import io.hyscale.generator.services.exception.ManifestErrorCodes;
 import io.hyscale.generator.services.json.JsonTreeOperations;
 import io.hyscale.generator.services.json.JsonTreeUtil;
-import io.hyscale.generator.services.model.ParentNodeContext;
 
 @Component
 public class ManifestTreeUtils {
 
-	private ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
-	@Autowired
-	private JsonTreeOperations jsonTreeOperations;
+    @Autowired
+    private JsonTreeOperations jsonTreeOperations;
 
-	@PostConstruct
-	public void init() {
-		this.objectMapper = ObjectMapperFactory.yamlMapper();
-	}
+    @PostConstruct
+    public void init() {
+        this.objectMapper = ObjectMapperFactory.yamlMapper();
+    }
 
-	public JsonNode injectSnippet(String snippet, String path, ObjectNode manifestRootNode)
-			throws IOException, HyscaleException {
-		if (StringUtils.isBlank(snippet)) {
-			return manifestRootNode;
-		}
-		String parentKey = JsonTreeUtil.getParentKey(path);
-		JsonNode snippetNode = objectMapper.readTree(snippet);
-		if (parentKey == null) {
-			parentKey = "$";
-			return jsonTreeOperations.put(manifestRootNode, parentKey, JsonTreeUtil.getKey(path), snippetNode);
-		}
-		ParentNodeContext parentContext = createParentsIfNotExists(manifestRootNode, JsonTreeUtil.getParentKey(path));
-		if (parentContext != null && parentContext.getRoot() != null) {
-			manifestRootNode = (ObjectNode) parentContext.getRoot();
-			// TODO check if the parent node is an arraynode or not , if yes add the element
-			// to the node if not inject
-			manifestRootNode = (ObjectNode) createLeafNodeIfNotExists(manifestRootNode, snippetNode, path);
-			if (parentContext.getParentNode() != null) {
-				return jsonTreeOperations.put(manifestRootNode, parentKey, JsonTreeUtil.getKey(path), snippetNode);
-			} else {
-				throw new HyscaleException(ManifestErrorCodes.ERROR_WHILE_INJECTING_MANIFEST_SNIPPET);
-			}
-		} else {
-			throw new HyscaleException(ManifestErrorCodes.ERROR_WHILE_INJECTING_MANIFEST_SNIPPET);
-		}
-	}
+    public JsonNode injectSnippet(String snippet, String path, ObjectNode rootNode)
+            throws IOException, HyscaleException {
+        if (StringUtils.isBlank(snippet)) {
+            return rootNode;
+        }
+        String parentKey = JsonTreeUtil.getParentKey(path);
+        JsonNode elementNode = objectMapper.readTree(snippet);
+        if (parentKey == null) {
+            parentKey = "$";
+            return jsonTreeOperations.put(rootNode, parentKey, JsonTreeUtil.getKey(path), elementNode);
+        }
+        rootNode = (ObjectNode) createParentsIfNotExists(rootNode,
+                JsonTreeUtil.getParentKey(path).split("\\."), 0);
+        rootNode = (ObjectNode) createLeafNodeIfNotExists(rootNode, elementNode, path);
+        return insertSnippet(rootNode, path, elementNode);
+    }
 
-	/**
-	 * Prepares the parent node of the path if array => prepare the array node of
-	 * the path and => if indexed , prepares the respective index element => prepare
-	 * the object node of the path
-	 *
-	 * @param jsonNode
-	 * @param field
-	 * @return
-	 * @throws IOException
-	 */
+    private JsonNode insertSnippet(ObjectNode rootNode, String path, JsonNode elementNode) throws HyscaleException, IOException {
+        JsonNode leafNode = jsonTreeOperations.read(rootNode, path);
+        if (leafNode == null) {
+            throw new HyscaleException(ManifestErrorCodes.ERROR_WHILE_INJECTING_MANIFEST_SNIPPET);
+        }
+        if (leafNode.isArray()) {
+            if (elementNode.isArray()) {
+                ArrayNode arrayNode = (ArrayNode) elementNode;
+                for (int i = 0; i < arrayNode.size(); i++) {
+                    rootNode = (ObjectNode) jsonTreeOperations.add(rootNode, path, arrayNode.get(i));
+                }
+            } else {
+                rootNode = (ObjectNode) jsonTreeOperations.add(rootNode, path, elementNode);
+            }
+        } else {
+            rootNode = jsonTreeOperations.put(rootNode, JsonTreeUtil.getParentKey(path), JsonTreeUtil.getKey(path), elementNode);
+        }
+        return rootNode;
+    }
 
-	private ParentNodeContext createParentsIfNotExists(JsonNode jsonNode, String field) throws IOException {
-		if (field == null) {
-			return null;
-		}
-		if (!field.startsWith("$")) {
-			field = "$." + field;
-		}
-		String[] paths = field.substring(2).split("\\.");
-		// As $ will be the first path
-		String effectivePath = "$";
-		JsonNode parent = jsonNode;
-		for (int i = 0; i < paths.length; i++) {
-			effectivePath += "." + paths[i];
+    /**
+     * Prepares the parent node of the path if array => prepare the array node of
+     * the path and => if indexed , prepares the respective index element => prepare
+     * the object node of the path
+     *
+     * @param root
+     * @param paths
+     * @param  index
+     * @return
+     * @throws IOException
+     */
 
-			parent = jsonTreeOperations.read(jsonNode, effectivePath);
-			if (parent == null) {
-				if (JsonTreeUtil.isArrayPath(paths[i])) {
-					parent = jsonTreeOperations.arrayNode();
-					jsonNode = jsonTreeOperations.put(jsonNode, JsonTreeUtil.getParentKey(effectivePath),
-							JsonTreeUtil.getSanitizedArrayPath(paths[i]), parent);
-					int index = JsonTreeUtil.getArrayIndex(paths[i]);
-					if (index >= 0) {
-						for (int j = 0; j <= index; j++) {
-							jsonNode = jsonTreeOperations.add(jsonNode,
-									JsonTreeUtil.getSanitizedArrayPath(effectivePath), jsonTreeOperations.objectNode());
-						}
-					}
-				} else {
+    private JsonNode createParentsIfNotExists(JsonNode root, String[] paths, int index) throws IOException {
+        if (paths == null) {
+            return root;
+        }
+        if (index >= paths.length) {
+            return root;
+        }
+        String effectivePath = "$";
+        for (int i = 0; i <= index; i++) {
+            effectivePath += "." + paths[i];
+        }
+        JsonNode parent = jsonTreeOperations.read(root, effectivePath);
+        if (parent == null) {
+            if (JsonTreeUtil.isArrayPath(paths[index])) {
+                parent = jsonTreeOperations.arrayNode();
+                root = jsonTreeOperations.put(root, JsonTreeUtil.getParentKey(effectivePath),
+                        JsonTreeUtil.getSanitizedArrayPath(paths[index]), parent);
+                int arrayIndex = JsonTreeUtil.getArrayIndex(paths[index]);
+                if (arrayIndex >= 0) {
+                    for (int j = 0; j <= arrayIndex; j++) {
+                        root = jsonTreeOperations.add(root,
+                                JsonTreeUtil.getSanitizedArrayPath(effectivePath), jsonTreeOperations.objectNode());
+                    }
+                }
+            } else {
+                parent = jsonTreeOperations.objectNode();
+                root = jsonTreeOperations.put(root, JsonTreeUtil.getParentKey(effectivePath), paths[index],
+                        parent);
+            }
+        }
+        return createParentsIfNotExists(root, paths, index + 1);
+    }
 
-					parent = jsonTreeOperations.objectNode();
-					jsonNode = jsonTreeOperations.put(jsonNode, JsonTreeUtil.getParentKey(effectivePath), paths[i],
-							parent);
-				}
-			}
-		}
-		return new ParentNodeContext(jsonNode, parent);
-	}
-
-	/*
-	 * Prepares the leaf node to inject the snippet Checks if the leaf node to be
-	 * inserted in an array, => if yes inserts arraynode => else inserts objectnode
-	 */
-	private JsonNode createLeafNodeIfNotExists(JsonNode root, JsonNode leafNode, String path) throws IOException {
-		if (jsonTreeOperations.read(root, path) == null) {
-			JsonNode leafParent = null;
-			if (leafNode.isArray()) {
-				leafParent = jsonTreeOperations.arrayNode();
-			} else {
-				leafParent = jsonTreeOperations.objectNode();
-			}
-			root = jsonTreeOperations.put(root, JsonTreeUtil.getParentKey(path), JsonTreeUtil.getKey(path), leafParent);
-		}
-		return root;
-	}
+    /*
+     * Prepares the leaf node to inject the snippet Checks if the leaf node to be
+     * inserted in an array, => if yes inserts arraynode => else inserts objectnode
+     */
+    private JsonNode createLeafNodeIfNotExists(JsonNode root, JsonNode elementNode, String path) throws IOException {
+        if (path == null || path.isBlank()) {
+            return root;
+        }
+        JsonNode leafNode = jsonTreeOperations.read(root, path);
+        if (leafNode == null) {
+            leafNode = elementNode.isArray() ? jsonTreeOperations.arrayNode() : jsonTreeOperations.objectNode();
+            root = jsonTreeOperations.put(root, JsonTreeUtil.getParentKey(path), JsonTreeUtil.getKey(path), leafNode);
+        }
+        return root;
+    }
 
 }
