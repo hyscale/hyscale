d8d16466d98c2ff77bc7105629f4338e27ea2212
tushar-tushar <tushar@hyscale.io>
2019-11-18 11:23:04 +0530

    Removing old references

--- image-builder/builder-services/src/main/java/io/hyscale/builder/services/config/ImageBuilderConfig.java
+++ image-builder/builder-services/src/main/java/io/hyscale/builder/services/config/ImageBuilderConfig.java
@@ -23,7 +23,7 @@ import io.hyscale.commons.config.SetupConfig;
 @Component
 public class ImageBuilderConfig {
 
-	public static final String IMAGE_BUILDER_PROP = "hyscalctl.image.builder";
+	public static final String IMAGE_BUILDER_PROP = "hyscale.image.builder";
 	private static final String PUSH_LOG = "push.log";
 	private static final String BUILD_LOG = "build.log";
 	@Autowired
@@ -31,7 +31,7 @@ public class ImageBuilderConfig {
 
 	public String getDockerBuildlog(String appName, String serviceName) {
 		StringBuilder sb = new StringBuilder(setupConfig.getLogsDir(appName, serviceName));
-		sb.append(BUILD_LOG);
+		sb.append(BUILD_LOG);	
 		return sb.toString();
 	}
 
