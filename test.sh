7d783c8fc59ad80771b5fc302022a602ec42af44
srujankujmar <srujan.gourishetty@hyscal.io>
2020-03-13 15:07:21 +0530

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-sed -i "s|@@BUILD_NUMBER@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
-sed -i "s|@@HYSCALE_BUILD_VERSION@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
-cp /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/Dockerfile /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
-cd /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
+#sed -i "s|@@BUILD_NUMBER@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
+#sed -i "s|@@HYSCALE_BUILD_VERSION@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
+#cp /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/Dockerfile /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
+#cd /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
 docker build --pull -t gcr.io/hyscale-dev-env/hyscale/hyscale:${Hyscale_Docker_Image_Tag}.$BUILD_NUMBER .
 docker rmi -f gcr.io/hyscale-dev-env/hyscale/hyscale:${Hyscale_Docker_Image_Tag}.$BUILD_NUMBER
