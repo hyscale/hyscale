f086ef124261201ee0d3fbe2ee7fdf841abe21d8
srujankujmar <srujan.gourishetty@hyscal.io>
2020-03-13 15:36:26 +0530

    test

--- test.sh
+++ test.sh
@@ -2,6 +2,5 @@
 #sed -i "s|@@HYSCALE_BUILD_VERSION@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
 #cp /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/Dockerfile /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
 #cd /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
-docker build --pull -t gcr.io/hyscale-dev-env/hyscale/hyscale:test.$BUILD_NUMBER .
+daocker build --pull -t gcr.io/hyscale-dev-env/hyscale/hyscale:test.$BUILD_NUMBER .
 docker rmi -f gcr.io/hyscale-dev-env/hyscale/hyscale:test.$BUILD_NUMBER
-echo $BUILD_NUMBER
