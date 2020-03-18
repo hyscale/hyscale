664fb2c5aae57e4bbc1866d79194a8d87d2db385
srujankujmar <srujan.gourishetty@hyscale.io>
2020-03-18 12:03:25 +0530

    test

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 #sed -i "s|@@HYSCALE_BUILD_VERSION@@|$Hyscale_Docker_Image_Tag.$BUILD_NUMBER|g" /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/scripts/*
 #cp /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-tool-ops/Dockerfile /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
 #cd /var/jenkins_home/hyscale/$BUILD_NUMBER/hyscale-ctl/
-docker build --pull -t registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER .
+#docker build  -t registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER .
 echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-docker push registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER
+#docker push registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER
 echo "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
-docker rmi -f registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER
+#docker rmi -f registry.hyscale.io/hyscale/hyscale:test.$BUILD_NUMBER
