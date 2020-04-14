b9618db07fa48c7d51320f2318915096dba11eab
satya <satya.prakash@hyscale.io>
2020-04-14 20:25:58 +0530

    added unit test for docker and registry validator

--- workflow-controller/controller-service/src/test/java/io/hyscale/controller/hooks/DockerDaemonValidatorTest.java
+++ workflow-controller/controller-service/src/test/java/io/hyscale/controller/hooks/DockerDaemonValidatorTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019 Pramati Prism, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.hyscale.controller.hooks;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,8 +24,9 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import io.hyscale.commons.exception.HyscaleException;
 import io.hyscale.controller.model.WorkflowContext;
@@ -19,29 +35,35 @@ import io.hyscale.controller.validator.impl.DockerDaemonValidator;
 @SpringBootTest
 public class DockerDaemonValidatorTest {
 
-	@Autowired
+    @MockBean
 	private DockerDaemonValidator dockerDaemonValidator;
+    
+    @MockBean
+    private WorkflowContext context;
 
 	public static Stream<Arguments> input() {
 		WorkflowContext context = new WorkflowContext();
 		return Stream.of(Arguments.of(context));
 	}
+	
 
 	@ParameterizedTest
 	@MethodSource(value = "input")
 	void testValidateForTrue(WorkflowContext context) {
 		try {
+			Mockito.when(dockerDaemonValidator.validate(context)).thenReturn(true);
 			assertTrue(dockerDaemonValidator.validate(context));
 		} catch (HyscaleException e) {
 			fail();
 		}
+		
 	}
 
 	@ParameterizedTest
 	@MethodSource(value = "input")
 	void testValidateForFalse(WorkflowContext context) {
 		try {
-			//TODO here we have to pass wrong context
+			Mockito.when(dockerDaemonValidator.validate(context)).thenReturn(false);
 			assertFalse(dockerDaemonValidator.validate(context));
 		} catch (HyscaleException e) {
 			fail();
--- workflow-controller/controller-service/src/test/java/io/hyscale/controller/hooks/RegistryValidatorTest.java
+++ workflow-controller/controller-service/src/test/java/io/hyscale/controller/hooks/RegistryValidatorTest.java
@@ -1,8 +1,71 @@
+/**
+ * Copyright 2019 Pramati Prism, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.hyscale.controller.hooks;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import io.hyscale.commons.exception.HyscaleException;
+import io.hyscale.controller.model.WorkflowContext;
+import io.hyscale.controller.util.ServiceSpecTestUtil;
+import io.hyscale.controller.validator.impl.RegistryValidator;
+import io.hyscale.servicespec.commons.model.service.BuildSpec;
+import io.hyscale.servicespec.commons.model.service.ServiceSpec;
 
 @SpringBootTest
 public class RegistryValidatorTest {
+
+	@Mock
+	private WorkflowContext context;
+	@Mock
+	private BuildSpec buildSpec;
+	@MockBean
+	private RegistryValidator registryValidator;
+	
+	public static Stream<Arguments> input() {
+		WorkflowContext context = new WorkflowContext();
+		return Stream.of(Arguments.of(context));
+	}
+
+	
+	@ParameterizedTest
+	@MethodSource(value = "input")
+	void testInvalidValidate(WorkflowContext context) {
+		ServiceSpec serviceSpec = null;
+		try {
+			serviceSpec = ServiceSpecTestUtil.getServiceSpec("/servicespecs/validator/registry_validation.hspec");
+		} catch (IOException e1) {
+			fail();
+		}
+		context.setServiceSpec(serviceSpec);
+		try {
+			assertFalse(registryValidator.validate(context));
+		} catch (HyscaleException e) {
+			fail();
+		}
+	}
 
 }
