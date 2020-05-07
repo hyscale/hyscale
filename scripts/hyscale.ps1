684753cecf70579fd68f133b664b2473ce0275e7
srujankujmar <srujan.gourishetty@hyscale.io>
2020-05-07 21:20:33 +0530

    Binary changes for windows and MAC

--- scripts/hyscale.ps1
+++ scripts/hyscale.ps1
@@ -1,13 +1,14 @@
-$HYS_VERSION="@@HYSCALE_BUILD_VERSION@@"
+$HYS_VERSION="@@HYS_BUILD_VERSION@@"
 $HYS_JAR_BIN="hyscale-${HYS_VERSION}.jar"
-$dir= "$env:LOCALAPPDATA\hyscale"
+$dir= "$home\.hyscale"
 $fileToCheck = "$dir\$HYS_JAR_BIN"
+$HYS_DOWNLOAD_URL = "@@HYS_URL@@"
 
 Function check_java_version
 {
   java --version | Out-Null
   If (-NOT  ($? -eq "True")) {
-    'Java is not installed'
+    'Either Java is not installed or found a lesser version of java'
      exit 1
 
     }
@@ -28,27 +29,9 @@ Function download_hyscale_jar
   if (!(Test-Path $fileToCheck -PathType leaf))
   {
      rm $dir\hyscale-*.jar
-     "Downloading jar file"
-     $url="https://github.com/hyscale/hyscale/releases/latest/download/hyscale.jar" 
-     try {
-       $checkConnection = Invoke-WebRequest -Uri $url -UseBasicParsing
-       if ($checkConnection.StatusCode -eq 200) {
-         Write-Host "Connection Verified!" -ForegroundColor Green
-         wget $url -outfile $HYS_JAR_BIN | Out-Null
-       }
-
-     } 
-     catch [System.Net.WebException] {
-       $exceptionMessage = $Error[0].Exception
-       if ($exceptionMessage -match "503") {
-         Write-Host "Server Unavaiable" -ForegroundColor Red
-         exit 1
-       }
-       elseif ($exceptionMessage -match "404") {
-         Write-Host "Page Not found" -ForegroundColor Red
-         exit 1
-       }  
-     }
+     "Downloading hyscale ..."
+     wget $HYS_DOWNLOAD_URL -outfile $HYS_JAR_BIN | Out-Null
+     "Download successful"
      mv $HYS_JAR_BIN $dir
   }
 }
@@ -57,4 +40,3 @@ check_java_version
 download_hyscale_jar
 
 java -Xms216m -Xmx512m -jar $dir\$HYS_JAR_BIN $args
-
