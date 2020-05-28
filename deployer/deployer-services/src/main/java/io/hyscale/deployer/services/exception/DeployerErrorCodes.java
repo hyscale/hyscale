07793897c8a90fc43dd21e95cb7294b3e1b2e600
sameerag <sameera.grandhi@hyscale.io>
2020-05-28 17:38:05 +0530

    Fixed issues when scaling is done out of bounds of HPA range

--- deployer/deployer-services/src/main/java/io/hyscale/deployer/services/exception/DeployerErrorCodes.java
+++ deployer/deployer-services/src/main/java/io/hyscale/deployer/services/exception/DeployerErrorCodes.java
@@ -57,7 +57,8 @@ public enum DeployerErrorCodes implements HyscaleErrorCode {
     SERVICE_NOT_DEPLOYED("{} is not deployed in namespace {} under app {} "),
     CANNOT_SCALE_NEGATIVE("INVALID VALUE {} : must be greater than or equal to 0"),
     CANNOT_SCALE_DOWN_ZERO("INVALID VALUE {} : scale down by value must be greater than 0"),
-    TIMEOUT_WHILE_WAITING_FOR_DEPLOYMENT("Timedout while waiting for deployment");
+    TIMEOUT_WHILE_WAITING_FOR_DEPLOYMENT("Timedout while waiting for deployment"),
+    CANNOT_SCALE_OUT_RANGE_HPA("Operation involves scaling out of the autoscaling range [{}-{}] configured");
 
     private String message;
 
