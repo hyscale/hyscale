89d2e86a43eff3911d376c231886bc0ebf96ad02
sameerag <sameera.grandhi@hyscale.io>
2020-07-10 13:00:33 +0530

    Resolving code review comments

--- cmd/root.go
+++ cmd/root.go
@@ -16,14 +16,10 @@ Copyright 2019 Pramati Prism, Inc.
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var (
@@ -62,33 +58,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	RootCmd.Flags().BoolVarP(&version, "version", "v", false, "Hyscale Version")
-}
-
-// initConfig reads in config file and ENV variables if set.
-//TODO Remove Viper
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cmd" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cmd")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
--- pkg/container/container.go
+++ pkg/container/container.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2019 Pramati Prism, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package container
 
 import (
@@ -10,6 +26,7 @@ import (
 	"github.com/docker/docker/api/types/mount"
 
 	"hyscale/pkg/constants"
+	cnst "hyscale/pkg/constants"
 	installer "hyscale/pkg/installer"
 	"hyscale/pkg/registry"
 	util "hyscale/pkg/util"
@@ -38,7 +55,7 @@ const (
 
 //TODO build image name with property
 var (
-	imageName      = "docker.io/hyscale/hyscale:0.9.6.0.39"
+	imageName      = "docker.io/hyscale/hyscale:"+cnst.ImageTag
 	currentUser, _ = user.Current()
 	dockerConf     = currentUser.HomeDir + configJSONPath
 	dockerConfDir  = currentUser.HomeDir + configDir
