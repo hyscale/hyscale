9802400534810af6364b5df2907c53028c41fd1a
tushar-tushar <tushar@hyscale.io>
2020-07-15 11:50:48 +0530

    manifest generator test: pending plugin handlers

---
+++ manifest-generator/generator-services/src/test/java/io/hyscale/generator/services/plugins/ServiceTypeTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2019 Pramati Prism, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hyscale.generator.services.plugins;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.hyscale.commons.exception.HyscaleException;
+import io.hyscale.commons.models.K8sServiceType;
+import io.hyscale.commons.models.ManifestContext;
+import io.hyscale.generator.services.model.ManifestResource;
+import io.hyscale.generator.services.utils.ManifestContextTestUtil;
+import io.hyscale.generator.services.utils.ServiceSpecTestUtil;
+import io.hyscale.plugin.framework.models.ManifestSnippet;
+import io.hyscale.servicespec.commons.model.service.ServiceSpec;
+
+@SpringBootTest
+class ServiceTypeTest {
+
+    @Autowired
+    private ServiceTypeHandler serviceTypeHandler;
+
+    private static Stream<Arguments> input() throws HyscaleException {
+        return Stream.of(Arguments.of(ServiceSpecTestUtil.getServiceSpec("/plugins/service/no-service.hspec"), null),
+                Arguments.of(ServiceSpecTestUtil.getServiceSpec("/plugins/service/external.hspec"),
+                        K8sServiceType.LOAD_BALANCER.getName()),
+                Arguments.of(ServiceSpecTestUtil.getServiceSpec("/plugins/service/internal.hspec"),
+                        K8sServiceType.CLUSTER_IP.getName()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    void testServiceTypeHandler(ServiceSpec serviceSpec, String expectedServiceType)
+            throws HyscaleException, IOException {
+        List<ManifestSnippet> manifestList = serviceTypeHandler.handle(serviceSpec, null);
+
+        if (StringUtils.isBlank(expectedServiceType)) {
+            assertTrue(CollectionUtils.isEmpty(manifestList));
+            return;
+        }
+
+        assertTrue(manifestList.stream().allMatch(manifest -> manifest.getPath().equals("spec.type")
+                && manifest.getSnippet().equals(expectedServiceType)));
+    }
+}
---
+++ manifest-generator/generator-services/src/test/java/io/hyscale/generator/services/plugins/StartCommandHandlerTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2019 Pramati Prism, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hyscale.generator.services.plugins;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.hyscale.commons.exception.HyscaleException;
+import io.hyscale.commons.models.ManifestContext;
+import io.hyscale.generator.services.model.ManifestResource;
+import io.hyscale.generator.services.utils.ManifestContextTestUtil;
+import io.hyscale.generator.services.utils.ServiceSpecTestUtil;
+import io.hyscale.plugin.framework.models.ManifestSnippet;
+import io.hyscale.servicespec.commons.fields.HyscaleSpecFields;
+import io.hyscale.servicespec.commons.model.service.ServiceSpec;
+
+@SpringBootTest
+class StartCommandHandlerTest {
+
+    @Autowired
+    private StartCommandHandler startCommandHandler;
+
+    private List<String> PATHS = Arrays.asList("spec.template.spec.containers[0].command",
+            "spec.template.spec.containers[0].args");
+
+    private static Stream<Arguments> input() throws HyscaleException {
+        return Stream.of(
+                Arguments.of(ServiceSpecTestUtil.getServiceSpec("/plugins/start-cmd/no-start-cmd.hspec"),
+                        ManifestContextTestUtil.getManifestContext(ManifestResource.STATEFUL_SET)),
+                Arguments.of(ServiceSpecTestUtil.getServiceSpec("/plugins/start-cmd/start-cmd.hspec"),
+                        ManifestContextTestUtil.getManifestContext(ManifestResource.STATEFUL_SET)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    void testStartCommandHandler(ServiceSpec serviceSpec, ManifestContext context)
+            throws HyscaleException, IOException {
+        List<ManifestSnippet> manifestList = startCommandHandler.handle(serviceSpec, context);
+
+        String startCommand = serviceSpec.get(HyscaleSpecFields.startCommand, String.class);
+
+        if (StringUtils.isBlank(startCommand)) {
+            assertTrue(CollectionUtils.isEmpty(manifestList));
+            return;
+        }
+
+        List<String> manifestPaths = manifestList.stream().map(manifest -> manifest.getPath())
+                .collect(Collectors.toList());
+
+        assertTrue(CollectionUtils.isEqualCollection(PATHS, manifestPaths));
+
+    }
+}
