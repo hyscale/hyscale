e74bef8a4e0cb45b4022c8f4cdb34c98c8d5f996
priyad <shiva.priya@hyscale.io>
2021-01-20 12:59:37 +0530

    Added getPlural function

--- commons/src/main/java/io/hyscale/commons/utils/HyscaleStringUtil.java
+++ commons/src/main/java/io/hyscale/commons/utils/HyscaleStringUtil.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2019 Pramati Prism, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,8 @@ import org.apache.commons.lang3.StringUtils;
 
 public class HyscaleStringUtil {
 
-    private HyscaleStringUtil() {}
+    private HyscaleStringUtil() {
+    }
 
     public static String removeSuffixStr(String input, String trailingStr) {
         if (StringUtils.isBlank(input) || StringUtils.isBlank(trailingStr)) {
@@ -43,6 +44,29 @@ public class HyscaleStringUtil {
             return null;
         }
         return removeSuffixStr(input.toString(), Character.toString(trailingChar));
+    }
+
+
+    public static String getPlural(String word) {
+        String consonants = "bcdfghjklmnpqrstvwxyzs";
+        boolean hasConsonant = consonants.contains(String.valueOf(word.charAt(word.length() - 2)));
+        String lastLetter = word.substring(0, word.length() - 1);
+        if (word.endsWith("s") || word.endsWith("x") || word.endsWith("z") || word.endsWith("ch") || word.endsWith("sh")) {
+            return word + "es";
+        }
+        if (word.endsWith("y") && hasConsonant) {
+            return lastLetter + "ies";
+        }
+        if (word.endsWith("f")) {
+            return lastLetter + "ves";
+        }
+        if (word.endsWith("fe")) {
+            return word.substring(0, word.length() - 2) + "ves";
+        }
+        if (word.endsWith("o") && hasConsonant) {
+            return word + "es";
+        }
+        return word += "s";
     }
 
 }
