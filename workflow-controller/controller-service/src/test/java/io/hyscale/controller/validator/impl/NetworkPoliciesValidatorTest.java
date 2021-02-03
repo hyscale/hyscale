bd5dbaa989831e9186a9b5cd004b2d26045620d2
priyad <shiva.priya@hyscale.io>
2021-02-03 15:43:46 +0530

    Added Validator Test Cases

---
+++ workflow-controller/controller-service/src/test/java/io/hyscale/controller/validator/impl/NetworkPoliciesValidatorTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2019 Pramati Prism, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.hyscale.controller.validator.impl;
+
+import io.hyscale.commons.exception.HyscaleException;
+import io.hyscale.controller.model.WorkflowContext;
+import io.hyscale.controller.model.WorkflowContextBuilder;
+import io.hyscale.controller.util.ServiceSpecTestUtil;
+import io.hyscale.generator.services.exception.ManifestErrorCodes;
+import io.hyscale.servicespec.commons.model.service.ServiceSpec;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
+
+@SpringBootTest
+public class NetworkPoliciesValidatorTest {
+
+    @Autowired
+    NetworkPoliciesValidator networkPoliciesValidator;
+
+    private static final Logger logger = LoggerFactory.getLogger(NetworkPoliciesValidatorTest.class);
+
+    private static Stream<Arguments> input() throws HyscaleException {
+        try {
+            return Stream.of(
+                    Arguments.of(ServiceSpecTestUtil.getServiceSpec("/network-policies/input/input-1.hspec")),
+                    Arguments.of(ServiceSpecTestUtil.getServiceSpec("/network-policies/input/input-2.hspec")),
+                    Arguments.of(ServiceSpecTestUtil.getServiceSpec("/network-policies/input/input-3.hspec")),
+                    Arguments.of(ServiceSpecTestUtil.getServiceSpec("/network-policies/input/input-4.hspec")),
+                    Arguments.of(ServiceSpecTestUtil.getServiceSpec("/network-policies/input/input-5.hspec")));
+        } catch (Exception e) {
+            HyscaleException ex = new HyscaleException(e, ManifestErrorCodes.ERROR_WHILE_CREATING_MANIFEST);
+            logger.error("Error while generating Input Files", ex);
+            throw ex;
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "input")
+    void testNetworkPoliciesValidator(ServiceSpec serviceSpec) {
+        try {
+            WorkflowContext context = new WorkflowContextBuilder(null).withService(serviceSpec).get();
+            System.out.println(networkPoliciesValidator.validate(context));
+            Assertions.assertTrue(networkPoliciesValidator.validate(context));
+        } catch (Exception e) {
+
+        }
+    }
+}
