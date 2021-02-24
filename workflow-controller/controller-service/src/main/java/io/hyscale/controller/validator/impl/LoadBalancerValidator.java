dd392975a22182d522f336101e7f1f3562383a6e
nishanth07 <nishanth.panthangi@hyscale.io>
2021-02-24 12:26:48 +0530

    minor fixes

--- workflow-controller/controller-service/src/main/java/io/hyscale/controller/validator/impl/LoadBalancerValidator.java
+++ workflow-controller/controller-service/src/main/java/io/hyscale/controller/validator/impl/LoadBalancerValidator.java
@@ -88,7 +88,7 @@ public class LoadBalancerValidator implements Validator<WorkflowContext> {
         List<String> portNumbersList = new ArrayList<>();
         portList.forEach(each -> portNumbersList.add(each.getPort()));
         // Fetching agents ports
-        portNumbersList.addAll(getAgentPorts(serviceSpec));
+       // portNumbersList.addAll(getAgentPorts(serviceSpec));
         List<String> lbPorts = new ArrayList<>();
         loadBalancer.getMapping().forEach(e -> lbPorts.add(e.getPort()));
         for (String lbPort : lbPorts) {
@@ -106,11 +106,16 @@ public class LoadBalancerValidator implements Validator<WorkflowContext> {
         List<String> portNumbersList = new ArrayList<>();
         try {
             List<Agent> agents = serviceSpec.get(HyscaleSpecFields.agents, agentsList);
+            if(agents == null || agents.isEmpty()){
+                Collections.emptyList();
+            }
             agents.forEach((agent -> {
                 List<Port> ports = agent.getPorts();
-                ports.forEach((port)->{
-                    portNumbersList.add(port.getPort());
-                });
+                if(ports!=null && !ports.isEmpty()){
+                    ports.forEach((port)->{
+                        portNumbersList.add(port.getPort());
+                    });
+                }
             }));
             return portNumbersList;
         } catch (HyscaleException e) {
